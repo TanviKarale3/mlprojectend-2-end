ecbefb31740965b18e5eec47b9178019cc2c409b
Tanvi karale <119518074+TanviKarale3@users.noreply.github.com>
2024-06-05 17:03:41 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,7 +8,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-@dataclass
+@dataclass                     #inside the class u use init to define the variable but if u use dataclass u will be directly defining the variable
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
